fadb6cd8c82caad4e17083915625139de3fdb2b6
Paulo Sérgio <paulossjunior@gmail.com>
2013-08-30 16:58:09 -0300

    Atualização do caminho do servidor.

--- ServerAPI/src/main/java/br/edu/ifes/sr/poo2/api/generic/APIGeneric.java
+++ ServerAPI/src/main/java/br/edu/ifes/sr/poo2/api/generic/APIGeneric.java
@@ -6,7 +6,7 @@ import com.google.gson.Gson;
 
 public abstract class APIGeneric {
 	
-	protected String URL = "http://localhost:8080/QuizWebService/rest/";
+	protected String URL = "http://leds.sr.ifes.edu.br:8080/QuizWebService/rest/";
 	
 	protected Gson gson = new Gson();
 	
